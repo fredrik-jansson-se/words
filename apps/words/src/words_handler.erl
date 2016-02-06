d0de59594615712bb3cd7c6aa757060bfb657460
Fredrik Jansson <frjansso@cisco.com>
2016-02-06 22:01:19 +0100

    Can pass match start/end from the UI

--- apps/words/src/words_handler.erl
+++ apps/words/src/words_handler.erl
@@ -29,11 +29,20 @@ resource_exists(Req, State) ->
         end,
   {Res, Req, State}.
 
+qs_to_opts([{<<"match_start">>, <<"true">>}|T], Opts) ->
+  qs_to_opts(T, [match_start|Opts]);
+qs_to_opts([{<<"match_end">>, <<"true">>}|T], Opts) ->
+  qs_to_opts(T, [match_end|Opts]);
+qs_to_opts([H|T], Opts) ->
+  io:format("Skipping ~p~n", [H]),
+  qs_to_opts(T, Opts);
+qs_to_opts([], Opts) ->
+  Opts.
 
 get_word_results(Req, State) ->
   QS = cowboy_req:parse_qs(Req),
   io:format("QS: ~p~n", [QS]),
   Word = cowboy_req:binding(word, Req),
-  {ok, Words} = words:match(Word),
+  {ok, Words} = words:match(Word, qs_to_opts(QS, [])),
   Res = jsx:encode(Words),
   {Res, Req, State}.
